ae1af6fbc4e9357307c40673d795544872264894
Connor Hull <chull@optimum-solutions.com>
2018-09-26 16:51:28 -0500

    change port, pedantic things

--- server.go
+++ server.go
@@ -18,20 +18,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var config Configuration
+	var config configuration
 	_ = json.NewDecoder(file).Decode(&config)
-	token = config.Verification_token
+	token = config.VerificationToken
 
 	router := mux.NewRouter()
-	router.HandleFunc("/", PostMessage).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	router.HandleFunc("/", postMessage).Methods("POST")
+	log.Fatal(http.ListenAndServe(":9803", router))
 }
 
-func PostMessage(w http.ResponseWriter, r *http.Request) {
-	var message Message
+func postMessage(w http.ResponseWriter, r *http.Request) {
+	var message message
 	b, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal(b, &message)
-	challengeResponse := ChallengeResponse{Challenge: message.Challenge}
+	challengeResponse := challengeResponse{Challenge: message.Challenge}
 	if message.Token == token {
 		w.Header().Set("Content-Type", "application/json")
 		j, _ := json.Marshal(challengeResponse)
@@ -39,16 +39,16 @@ func PostMessage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type Message struct {
-	Token      string `json:"token,omitempty"`
-	Challenge  string `json:"challenge,omitempty"`
-	Event_type string `json:"type,omitempty"`
+type message struct {
+	Token     string `json:"token,omitempty"`
+	Challenge string `json:"challenge,omitempty"`
+	EventType string `json:"type,omitempty"`
 }
 
-type ChallengeResponse struct {
+type challengeResponse struct {
 	Challenge string `json:"challenge"`
 }
 
-type Configuration struct {
-	Verification_token string
+type configuration struct {
+	VerificationToken string
 }
