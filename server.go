4157b163ba9f071702096ab3f802ac808276bcc7
Connor Hull <chull@optimum-solutions.com>
2018-09-27 13:54:37 -0500

    pup plugin

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"strings"
@@ -15,6 +16,7 @@ import (
 
 var vtoken string
 var btoken string
+var imgurclient string
 
 func main() {
 	file, err := os.Open("conf.json")
@@ -26,6 +28,7 @@ func main() {
 	_ = json.NewDecoder(file).Decode(&config)
 	vtoken = config.VerificationToken
 	btoken = config.BotToken
+	imgurclient = config.ImgurClient
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", postMessage).Methods("POST")
@@ -41,7 +44,7 @@ func postMessage(w http.ResponseWriter, r *http.Request) {
 	case "url_verification":
 		urlVerification(w, r, m)
 	case "event_callback":
-		eventCallback(w, r, m)
+		go eventCallback(w, r, m)
 	default:
 		log.Print(string(b))
 	}
@@ -67,6 +70,34 @@ func handleMessage(m message) {
 	if strings.Contains(m.Event.Text, "fuck") {
 		postResponse(m.Event.Channel, "https://youtu.be/hpigjnKl7nI?t=2s")
 	}
+
+	if strings.Contains(m.Event.Text, "@B725HJ0DT") && strings.Contains(m.Event.Text, "pup") {
+		var j map[string]imgurresp
+		puppies := imgurAPI("dog")
+		json.NewDecoder(strings.NewReader(puppies)).Decode(&j)
+
+		rand.Seed(time.Now().Unix())
+		i := rand.Intn(len(j["data"].Items))
+
+		postResponse(m.Event.Channel, j["data"].Items[i].Link)
+	}
+}
+
+func imgurAPI(tag string) string {
+	url := "https://api.imgur.com/3/gallery/t/dog/top/day/"
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Authorization", "Client-ID "+imgurclient)
+
+	client := &http.Client{}
+	client.Timeout = time.Second * 15
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	return string(body)
 }
 
 func postResponse(channel string, text string) {
@@ -95,6 +126,7 @@ type message struct {
 	TeamID    string `json:"team_id"`
 	Event     event  `json:"event"`
 }
+
 type event struct {
 	Type        string `json:"type"`
 	User        string `json:"user"`
@@ -103,6 +135,14 @@ type event struct {
 	Time        string `json:"ts"`
 	Channel     string `json:"channel"`
 	ChannelType string `json:"channel_type"`
+}
+
+type imgurresp struct {
+	Items []imguritem `json:"items"`
+}
+
+type imguritem struct {
+	Link string `json:"link"`
 }
 
 type challengeResponse struct {
@@ -117,4 +157,5 @@ type reply struct {
 type configuration struct {
 	VerificationToken string `json:"verification_token"`
 	BotToken          string `json:"bot_token"`
+	ImgurClient       string `json:"imgur_client"`
 }
